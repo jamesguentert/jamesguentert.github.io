c470db9542ba8f73ab4c3621dadb993eb2504365
jamesguentert <60209200+jamesguentert@users.noreply.github.com>
2020-05-20 10:43:41 -0700

    Added new code to Code 2 and retitled buttons

--- uiaminationtest/scripts/secondcode.js
+++ uiaminationtest/scripts/secondcode.js
@@ -1,53 +1,25 @@
-let velocitAvg
-let Xval;
-let Yval;
-let CanvasSize;
-let size;
-let onetothree =[1,2,3]
-let velocity1;
-let velocity2;
-let pallet;
+var palletCoices=[1, 2, 3,4, 5, 6];
 function setup() {
   createCanvas(400, 400);
-  velocity1=random(-1,1)
-  velocity2=random(-1,1)
-  velocitAvg=(abs(velocity1)+abs(velocity2))/2
-  pallet=random(onetothree)
-  Xval=width/2
-  Yval=width/2
+  pallet = random(palletCoices)
 }
-
-
+var i =100
 function draw() {
-background(0)
-if (pallet ==1) fill(Xval,Yval,(-1*(velocity2)/velocity1)*50)
-else if (pallet ==2) fill((-1*(velocity2)/velocity1)*50,Xval,Yval)
-else fill(Yval,(-1*(velocity2)/velocity1)*50,Xval)
-velocitAvg=(abs(velocity1)+abs(velocity2))/2
-size=(width/4)*(0.5**velocitAvg)
-CanvasInvisableBoarder=width-(size/2)
-ellipse(Xval,Yval,size,size)
-
-if(Xval<=size/2 || Xval>=CanvasInvisableBoarder) {
-  Xvelocityreset()}
-else if(Yval<=size/2 || Yval>=CanvasInvisableBoarder) {
-  Yvelocityreset()}
-else move()
+  background(0);
+  for (var x=0; x<=width; x+=i){
+    if (pallet ==1)fill(i,x,0)
+    if (pallet ==2)fill(0,x,i)
+    if (pallet ==3)fill(x,i,x)
+    if (pallet ==4)fill(0,i,x)
+    if (pallet ==5)fill(x,0,i)
+    if (pallet ==6)fill(i,0,x)
+    ellipse(x,x,x)
 }
-
-function move(){
-Xval+=velocity1
-Yval+=velocity2
-
+if (i > 1){
+i-=0.5
 }
-function Xvelocityreset(){
-velocity1*=-1.05
-pallet=random(onetothree)
-move()
-if (velocity1>1000 || velocity1<-1000) velocity1=10
+else {
+i=100
+pallet = random(palletCoices)
 }
-function Yvelocityreset(){
-velocity2*=-1.05
-if (velocity2>1000 || velocity2<-1000) velocity2=10
-pallet=random(onetothree)
-move()}
+}
