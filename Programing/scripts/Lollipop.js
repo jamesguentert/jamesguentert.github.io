7a5552158d07cf194e96eb9104ec0cd45b5162f4
jamesguentert <60209200+jamesguentert@users.noreply.github.com>
2020-05-26 18:25:44 -0700

    Major visual changes

--- Programing/scripts/Lollipop.js
+++ Programing/scripts/Lollipop.js
@@ -3,12 +3,12 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-lollipop(mouseX,mouseY,100,0,100)
+  background(0);
+  lollipop(mouseX,mouseY,200,0,100)
 }
 function lollipop(x,y,color1,color2,color3) {
-fill(0)
-rect(x-5,y,10,100)
-fill(color1,color2,color3)
-ellipse(x,y,50,50);
+  fill(255);
+  rect(x-5,y,10,100)
+  fill(color1,color2,color3)
+  ellipse(x,y,50,50);
 }
